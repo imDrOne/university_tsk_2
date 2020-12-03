2800732266f8f655849591596dedc785d2703f8d
DrOne <apelsintih@mail.ru>
2020-12-03 16:50:50 +0300

    add first test method finder;

---
+++ src/main/java/ru/miit/interfaces/IGraphable.java
@@ -0,0 +1,9 @@
+package ru.miit.interfaces;
+
+import ru.miit.utils.Node;
+
+import java.util.ArrayList;
+
+public interface IGraphable {
+    public String find(int adjacencyMatrix[][]);
+}
---
+++ src/main/java/ru/miit/utils/Node.java
@@ -0,0 +1,10 @@
+package ru.miit.utils;
+
+public class Node {
+    public static int data;
+    public static boolean visited;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
